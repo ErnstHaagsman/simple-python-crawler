4d8eaf310994898933e1c630b9708cb059c2239c
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2016-11-17 17:44:30 +0100

    Style fixes for Python 3.5

--- crawl.py
+++ crawl.py
@@ -5,10 +5,12 @@ from urllib.parse import urljoin, urlsplit
 import aiohttp
 from bs4 import BeautifulSoup
 
+
+__all__ = ['main']
 _crawled_pages = []
 
 
-def _checkLink(from_url, to_url):
+def check_link(from_url, to_url):
     # don't follow links out from the domain
     parts = urlsplit(to_url)
     if parts.hostname and parts.hostname != urlsplit(from_url).hostname:
@@ -26,8 +28,8 @@ def _checkLink(from_url, to_url):
     return True
 
 
-async def _handlePage(loop, session, url, depth_remaining):
-    print(str.format('Getting {}', url))
+async def handle_page(loop, session, url, depth_remaining):
+    print('Getting {}'.format(url))
     # get page
     async with session.get(url) as response:
         # get the page
@@ -41,8 +43,8 @@ async def _handlePage(loop, session, url, depth_remaining):
                 new_url = urljoin(url, link)
 
                 # check whether we want to follow the link
-                if _checkLink(url, new_url):
-                    await _handlePage(loop, session, new_url, depth_remaining - 1)
+                if check_link(url, new_url):
+                    await handle_page(loop, session, new_url, depth_remaining - 1)
 
         # print image links
         images = [img.get('src') for img in soup.find_all('img', src=True)]
@@ -50,14 +52,14 @@ async def _handlePage(loop, session, url, depth_remaining):
             print(urljoin(url, image))
 
 
-async def _crawl(loop, url, depth):
-    print(str.format('Parsing {} for {} level(s)', url, depth))
+async def crawl(loop, url, depth):
+    print('Parsing {} for {} level(s)'.format(url, depth))
 
     # run checklink to add initial page to the crawled pages list
-    _checkLink(url, url)
+    check_link(url, url)
 
     async with aiohttp.ClientSession(loop=loop) as session:
-        await _handlePage(loop, session, url, depth)
+        await handle_page(loop, session, url, depth)
 
 
 def main():
@@ -69,7 +71,7 @@ def main():
 
     # start crawling async
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(_crawl(loop, arguments.url, arguments.depth))
+    loop.run_until_complete(crawl(loop, arguments.url, arguments.depth))
 
 
 if __name__ == '__main__':
